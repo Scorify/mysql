882e9a132fe173989eaea548462feb6e09053230
1nv8rZim <msf9542@rit.edu>
2024-09-15 20:51:56 -0400

    implement check logic

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package mysql
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Schema struct {
@@ -20,6 +24,31 @@ func Run(ctx context.Context, config string) error {
 	err := json.Unmarshal([]byte(config), &schema)
 	if err != nil {
 		return err
+	}
+
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", schema.Username, schema.Password, schema.Target, schema.Port, schema.Database)
+
+	conn, err := sql.Open("mysql", connStr)
+	if err != nil {
+		return fmt.Errorf("failed to connect to mysql server: %w", err)
+	}
+	defer conn.Close()
+
+	err = conn.Ping()
+	if err != nil {
+		return fmt.Errorf("failed to ping mysql server: %w", err)
+	}
+
+	if schema.Query != "" {
+		rows, err := conn.Query(schema.Query)
+		if err != nil {
+			return fmt.Errorf("failed to execute query: %w", err)
+		}
+		defer rows.Close()
+
+		if !rows.Next() {
+			return fmt.Errorf("no rows returned from query: %q", schema.Query)
+		}
 	}
 
 	return nil
