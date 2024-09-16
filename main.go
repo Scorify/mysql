8d798c18422458b67378009b959ea426a3587ad9
1nv8rZim <msf9542@rit.edu>
2024-09-15 20:47:14 -0400

    Add check schema

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 type Schema struct {
-	Target string `json:"target"`
-	Port   int    `json:"port"`
+	Target   string `json:"target"`
+	Port     int    `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Database string `json:"database"`
+	Query    string `json:"query"`
 }
 
 func Run(ctx context.Context, config string) error {
