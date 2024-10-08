b134ee8cb30009216264e66190b2142471edf2f1
1nv8rZim <msf9542@rit.edu>
2024-10-07 21:19:05 -0400

    Add `Validate` function

--- main.go
+++ main.go
@@ -18,6 +18,37 @@ type Schema struct {
 	Query    string `key:"query"`
 }
 
+func Validate(config string) error {
+	conf := Schema{}
+
+	err := schema.Unmarshal([]byte(config), &conf)
+	if err != nil {
+		return err
+	}
+
+	if conf.Server == "" {
+		return fmt.Errorf("server is required; got %q", conf.Server)
+	}
+
+	if conf.Port <= 0 || conf.Port > 65535 {
+		return fmt.Errorf("port is invalid; got %d", conf.Port)
+	}
+
+	if conf.Username == "" {
+		return fmt.Errorf("username is required; got %q", conf.Username)
+	}
+
+	if conf.Password == "" {
+		return fmt.Errorf("password is required; got %q", conf.Password)
+	}
+
+	if conf.Database == "" {
+		return fmt.Errorf("database is required; got %q", conf.Database)
+	}
+
+	return nil
+}
+
 func Run(ctx context.Context, config string) error {
 	schema := Schema{}
 
